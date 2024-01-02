d7216857d042d0978c1b5190846f7c8fb6653325
Ansar Saeed <89566908+Ansar695@users.noreply.github.com>
2024-01-02 11:50:49 +0500

    mui integrated with vite reactjs

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-5">
+      <h1>Meterial UI Integrated with Vite ReactJS</h1>
+      <Button variant='contained'>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
